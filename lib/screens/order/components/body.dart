d8e77a40ff5dac3e3ada9ed2c7033f7c297f35be
PhuHuu <tuhu556@gmail.com>
2021-10-26 14:23:38 +0700

    update order screen

--- lib/screens/order/components/body.dart
+++ lib/screens/order/components/body.dart
@@ -8,34 +8,30 @@ import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Body extends StatefulWidget {
-  const Body({ Key? key }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   _BodyState createState() => _BodyState();
 }
 
 class _BodyState extends State<Body> {
-
-  
   late Future<List<Appointment>> _getAppointment;
 
   Future<List<Appointment>> getAppointment() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<Appointment> appointments = [];
     String? token = prefs.getString("token");
-    final url = Uri.parse('${Api.url}/appointmentCusEmail?cusEmail=${prefs.getString("email")}');
+    final url = Uri.parse(
+        '${Api.url}/appointmentCusEmail?cusEmail=${prefs.getString("email")}');
     Map<String, String> requestHeaders = {'Authorization': '$token'};
-    var response = await http.get(
-      url,
-      headers: requestHeaders
-    );
+    var response = await http.get(url, headers: requestHeaders);
     var jsonData = json.decode(response.body);
 
     for (var e in jsonData) {
       Appointment appointment = Appointment.formJson(e);
       appointments.add(appointment);
     }
-    appointments.sort((a,b) {
+    appointments.sort((a, b) {
       var adate = a.startTime ?? DateTime.now();
       var bdate = b.startTime ?? DateTime.now();
       return bdate.compareTo(adate);
@@ -52,13 +48,9 @@ class _BodyState extends State<Body> {
       "apptID": apptID,
       "status": "rejected",
     };
-    var response = await http.put(
-      url,
-      headers: requestHeaders,
-      body: body
-    );
+    var response = await http.put(url, headers: requestHeaders, body: body);
     var jsonData = json.decode(response.body);
-    
+
     if (response.statusCode == 200) {
       _pullRefresh();
     }
@@ -108,33 +100,28 @@ class _BodyState extends State<Body> {
               child: RefreshIndicator(
                 onRefresh: _pullRefresh,
                 child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      FutureBuilder(
-                        future: _getAppointment,
-                        builder: (BuildContext context, AsyncSnapshot snapshot) {
-                          if (snapshot.connectionState == ConnectionState.waiting) {
-                            return Container(margin: EdgeInsets.only(top: size.width / 2), child: Center(child: CircularProgressIndicator()));
-                          }
-                          if (snapshot.hasError) {
-                            return Center(
-                              child: Text('Error'),
-                            );
-                          } else
-                            return ListView.builder(
-                              itemCount: snapshot.data?.length,
-                              //physics: NeverScrollableScrollPhysics(),
-                              shrinkWrap: true,
-                              //primary: false,
-                              itemBuilder: (BuildContext context, int i) {
-                                return DetailBooking(snapshot.data[i]);
-                              });
-                            //return DetailBooking(snapshot.data[i]);
-                        },
-                      ),
-                    ],
+                  child: FutureBuilder(
+                    future: _getAppointment,
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text('Error'),
+                        );
+                      } else
+                        return ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: snapshot.data?.length,
+                            //physics: NeverScrollableScrollPhysics(),
+                            //primary: false,
+                            itemBuilder: (BuildContext context, int i) {
+                              return DetailBooking(snapshot.data[i]);
+                            });
+                      //return DetailBooking(snapshot.data[i]);
+                    },
                   ),
                   padding: EdgeInsets.symmetric(horizontal: 15),
                   decoration: BoxDecoration(
@@ -152,13 +139,15 @@ class _BodyState extends State<Body> {
       ),
     );
   }
-  
+
   Widget DetailBooking(Appointment appointment) {
     final dateFormatter = DateFormat('dd-MM-yyyy');
     final timeFormatter = DateFormat('HH:mm');
     final currencyFormatter = NumberFormat.currency(locale: 'vi');
-    final dateString = dateFormatter.format(appointment.startTime ?? DateTime.now());
-    final timeString = timeFormatter.format(appointment.startTime ?? DateTime.now());
+    final dateString =
+        dateFormatter.format(appointment.startTime ?? DateTime.now());
+    final timeString =
+        timeFormatter.format(appointment.startTime ?? DateTime.now());
     String statusText = "";
     int statusColor = 0xff242424;
     if (appointment.status == "ON PROCCESS") {
@@ -174,92 +163,125 @@ class _BodyState extends State<Body> {
       statusColor = 0xffff5e5e;
     }
     return Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          child: Image.asset("assets/images/logo.png", width: 60,),
-                          padding: const EdgeInsets.all(5),
-                          margin: const EdgeInsets.only(right: 10.0, bottom: 1.0),
-                          decoration: BoxDecoration(
-                            color: Color(0xffFFDFF2),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20.0),
-                            ),
-                          ),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      width: 60,
+                    ),
+                    padding: const EdgeInsets.all(5),
+                    margin: const EdgeInsets.only(right: 10.0, bottom: 1.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xffFFDFF2),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("AppointmentID",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              color: Color(0xff4B4B4B),
+                              fontSize: 16)),
+                      SizedBox(height: 5.5),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Date: ',
+                          style: TextStyle(
+                              color: Color(0xff9E9E9E),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12),
                           children: [
-                            Text("Barbershop", style: TextStyle(fontWeight: FontWeight.w800, color: Color(0xff4B4B4B), fontSize: 16)),
-                            SizedBox(height: 5.5),
-                            RichText(
-                              text: TextSpan(
-                                text: 'Date: ',
-                                style: TextStyle(color: Color(0xff9E9E9E), fontWeight: FontWeight.bold, fontSize: 12),
-                                children: [
-                                  TextSpan(text: '${dateString}', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12)),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 2.5),
-                            RichText(
-                              text: TextSpan(
-                                text: 'Time: ',
-                                style: TextStyle(color: Color(0xff9E9E9E), fontWeight: FontWeight.bold, fontSize: 12),
-                                children: [
-                                  TextSpan(text: '${timeString}', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12)),
-                                ],
-                              ),
-                            ),
+                            TextSpan(
+                                text: '${dateString}',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12)),
                           ],
                         ),
-                      ],
-                    ),
-                    appointment.status == "ON PROCCESS" ? OutlinedButton(
+                      ),
+                      SizedBox(height: 2.5),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Time: ',
+                          style: TextStyle(
+                              color: Color(0xff9E9E9E),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12),
+                          children: [
+                            TextSpan(
+                                text: '${timeString}',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              appointment.status == "ON PROCCESS"
+                  ? OutlinedButton(
                       onPressed: () {
                         cancelAppointment(appointment.apptID ?? "");
                       },
                       child: const Text('Cancel'),
-                    ) : Container(),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text("${statusText}", style: TextStyle(color: Color(statusColor), fontWeight: FontWeight.w900, fontSize: 14)),
-                    Text("${currencyFormatter.format(appointment.totalPrice)}", style: TextStyle(color: Color(0xffF88E79), fontWeight: FontWeight.w900, fontSize: 24))
-                  ],
-                )
-              ],
-            ),
-            padding: const EdgeInsets.only(top: 10.0, right: 15, bottom: 10, left: 15),
-            margin: const EdgeInsets.symmetric(vertical: 8.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(
-                Radius.circular(20.0),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  offset: Offset(0,5),
-                  blurRadius: 5.0,
-                  spreadRadius: 0
-                )
-              ],
-            ),
-          );
+                    )
+                  : Container(),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text("${statusText}",
+                  style: TextStyle(
+                      color: Color(statusColor),
+                      fontWeight: FontWeight.w900,
+                      fontSize: 14)),
+              Text("${currencyFormatter.format(appointment.totalPrice)}",
+                  style: TextStyle(
+                      color: Color(0xffF88E79),
+                      fontWeight: FontWeight.w900,
+                      fontSize: 24))
+            ],
+          )
+        ],
+      ),
+      padding:
+          const EdgeInsets.only(top: 10.0, right: 15, bottom: 10, left: 15),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              offset: Offset(0, 5),
+              blurRadius: 5.0,
+              spreadRadius: 0)
+        ],
+      ),
+    );
   }
 }
